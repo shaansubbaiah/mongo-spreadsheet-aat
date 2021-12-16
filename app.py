3808371d8ad646a0156735753b4db63d45cc0a19
Shaan Subbaiah B C <shaansubbaiah@yahoo.com>
2021-12-16 19:42:21 +0530

    Table pagination, filtering, styling

--- app.py
+++ app.py
@@ -22,16 +22,52 @@ db = client.myFirstDatabase
 recipesCollection = db.recipes
 
 # Create dataframe from collection
-recipesList = list(recipesCollection.find({}, {'_id': False}).limit(5))
+recipesList = list(recipesCollection.find({}, {'_id': False}).limit(20))
 
 recipesDF = pd.DataFrame(recipesList)
 
-print(recipesDF.columns)
-
-app.layout = dash_table.DataTable(
-    columns=[{"name": i, "id": i} for i in recipesDF.columns],
-    data = recipesList
-)
+app.layout = html.Div([
+    html.H1('READ'),
+    dash_table.DataTable(
+        columns=[{"name": i, "id": i} for i in recipesDF.columns],
+        data = recipesList,
+        page_size = 10,
+        fixed_columns={ 'headers': True, 'data': 1 },
+        style_table={'minWidth': '100%'},
+        style_cell={
+            # all three widths are needed
+            'minWidth': '180px', 'width': '180px', 'maxWidth': '180px',
+            'overflow': 'hidden',
+            'textOverflow': 'ellipsis',
+            'padding': '5px'
+        },
+        style_data_conditional=[
+            {
+                'if': {
+                    'column_id': 'name'
+                },
+                'minWidth': 'auto', 'width': 'auto', 'maxWidth': 'auto',
+                'fontWeight': 'bold'
+            },
+        ],
+        style_as_list_view=True,
+        style_header={
+            'backgroundColor': 'white',
+            'fontWeight': 'bold'
+        },
+        filter_action="native",
+        tooltip_data=[
+            {
+                column: {'value': str(value), 'type': 'markdown'}
+                for column, value in row.items()
+            } for row in recipesList
+        ],
+        css=[{
+            'selector': '.dash-table-tooltip',
+            'rule': 'font-family: monospace;'
+        }],
+    )
+])
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
